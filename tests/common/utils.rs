34ce4e5b4e51f2d659f17d9faf0521ca0a4893ab
Sosthene <contact@sosthene.net>
2024-10-30 12:57:19 +0100

    Fix tests

--- tests/common/utils.rs
+++ tests/common/utils.rs
@@ -1,8 +1,9 @@
 use std::{fs::File, io::Read, str::FromStr};
 
+use bitcoin::{consensus::serialize, secp256k1::{self, silentpayments::SilentpaymentsRecipient, Scalar, SecretKey, XOnlyPublicKey}, OutPoint, Txid};
 use bitcoin_hashes::Hash;
-use bitcoin::secp256k1::{self, Scalar, SecretKey, XOnlyPublicKey};
 use serde_json::from_str;
+use silentpayments::utils::SilentPaymentAddress;
 
 use super::structs::{OutputWithSignature, TestData};
 
@@ -35,6 +36,23 @@ fn deser_string(f: &mut Cursor<&Vec<u8>>) -> io::Result<Vec<u8>> {
     Ok(buf)
 }
 
+pub fn get_smallest_outpoint(outpoints: Vec<(String, u32)>) -> anyhow::Result<[u8; 36]> {
+    let mut sorted_outpoints = Vec::with_capacity(outpoints.len());
+    for (txid, vout) in outpoints {
+        let txid = Txid::from_str(&txid)?;
+        let outpoint = OutPoint::new(txid, vout);
+        sorted_outpoints.push(serialize(&outpoint));
+    }
+    sorted_outpoints.sort_unstable();
+
+    let smallest_outpoint = sorted_outpoints.first().take().unwrap();
+
+    let mut ret = [0u8; 36];
+    ret.copy_from_slice(&smallest_outpoint);
+
+    Ok(ret)
+}
+
 pub fn deser_string_vector(f: &mut Cursor<&Vec<u8>>) -> io::Result<Vec<Vec<u8>>> {
     // Check if the buffer is empty before attempting to deserialize the size
     if f.get_ref().is_empty() {
@@ -62,15 +80,21 @@ pub fn decode_outputs_to_check(outputs: &Vec<String>) -> Vec<XOnlyPublicKey> {
         .collect()
 }
 
-pub fn decode_recipients(recipients: &Vec<String>) -> Vec<String> {
+pub fn decode_recipients(recipients: Vec<String>) -> Vec<SilentpaymentsRecipient> {
     recipients
         .iter()
-        .map(|sp_addr_str| sp_addr_str.to_owned())
+        .enumerate()
+        .map(|(i, sp_addr_str)| {
+            let sp_addr = SilentPaymentAddress::try_from(sp_addr_str.as_str()).unwrap();
+
+            let recipient = SilentpaymentsRecipient::new(&sp_addr.get_scan_key(), &sp_addr.get_spend_key(), i);
+            recipient
+        })
         .collect()
 }
 
 pub fn verify_and_calculate_signatures(
-    key_tweaks: Vec<Scalar>,
+    key_tweaks: Vec<[u8; 32]>,
     b_spend: SecretKey,
 ) -> Result<Vec<OutputWithSignature>, secp256k1::Error> {
     let secp = secp256k1::Secp256k1::new();
@@ -81,7 +105,7 @@ pub fn verify_and_calculate_signatures(
     let mut res: Vec<OutputWithSignature> = vec![];
     for tweak in key_tweaks {
         // Add the tweak to the b_spend to get the final key
-        let k = b_spend.add_tweak(&tweak)?;
+        let k = b_spend.add_tweak(&Scalar::from_be_bytes(tweak).unwrap())?;
 
         // get public key
         let P = k.x_only_public_key(&secp).0;
@@ -95,7 +119,7 @@ pub fn verify_and_calculate_signatures(
         // Push result to list
         res.push(OutputWithSignature {
             pub_key: P.to_string(),
-            priv_key_tweak: hex::encode(tweak.to_be_bytes()),
+            priv_key_tweak: hex::encode(tweak),
             signature: sig.to_string(),
         });
     }
--- tests/vector_tests.rs
+++ tests/vector_tests.rs
@@ -2,23 +2,25 @@
 mod common;
 #[cfg(test)]
 mod tests {
-    use bitcoin::secp256k1::{PublicKey, Scalar, Secp256k1, SecretKey};
-    use silentpayments::{
-        receiving::Label,
-        utils::{
-            receiving::{
-                calculate_ecdh_shared_secret, calculate_tweak_data, get_pubkey_from_input, is_p2tr,
-            },
-            sending::calculate_partial_secret,
-            Network,
+    use bimap::BiMap;
+    use bitcoin::consensus::serialize;
+    use bitcoin::secp256k1::silentpayments::{
+        silentpayments_recipient_create_label_tweak, silentpayments_recipient_create_labelled_spend_pubkey, silentpayments_recipient_create_output_pubkey, silentpayments_recipient_scan_outputs, silentpayments_sender_create_outputs, LabelTweakResult, SilentpaymentsPublicData, SilentpaymentsRecipient
+    };
+    use bitcoin::secp256k1::{PublicKey, Scalar, Secp256k1, SecretKey, Keypair};
+    use bitcoin::{XOnlyPublicKey, OutPoint, Txid};
+    use silentpayments::utils::common::{label_lookup_callback, LabelsStore};
+    use silentpayments::utils::SilentPaymentAddress;
+    use silentpayments::utils::{
+        receiving::{
+            calculate_ecdh_shared_secret, calculate_tweak_data, get_pubkey_from_input, is_p2tr, is_eligible
         },
+        common::Label,
+        Network,
     };
     use std::{collections::HashSet, io::Cursor, str::FromStr};
 
-    use silentpayments::receiving::Receiver;
-
-    use silentpayments::sending::generate_recipient_pubkeys;
-
+    use crate::common::utils::get_smallest_outpoint;
     use crate::common::{
         structs::TestData,
         utils::{
@@ -28,6 +30,7 @@ mod tests {
     };
 
     const NETWORK: Network = Network::Mainnet;
+    const VERSION: u8 = 0;
 
     #[test]
     fn test_with_test_vectors() {
@@ -52,7 +55,8 @@ mod tests {
                 .iter()
                 .map(|vin| (vin.txid.clone(), vin.vout))
                 .collect();
-            let mut input_priv_keys = Vec::new();
+            let mut taproot_priv_keys: Vec<Keypair> = Vec::new();
+            let mut plain_priv_keys: Vec<SecretKey> = Vec::new();
             for input in given.vin {
                 let script_sig = hex::decode(&input.scriptSig).unwrap();
                 let txinwitness_bytes = hex::decode(&input.txinwitness).unwrap();
@@ -61,33 +65,55 @@ mod tests {
                 let script_pub_key = hex::decode(&input.prevout.scriptPubKey.hex).unwrap();
 
                 match get_pubkey_from_input(&script_sig, &txinwitness, &script_pub_key) {
-                    Ok(Some(_pubkey)) => input_priv_keys.push((
-                        SecretKey::from_str(&input.private_key).unwrap(),
-                        is_p2tr(&script_pub_key),
-                    )),
+                    Ok(Some((_pubkey, is_p2tr))) => {
+                        if is_p2tr {
+                            taproot_priv_keys.push(Keypair::from_secret_key(&secp, &SecretKey::from_str(&input.private_key).unwrap()))
+                        } else {
+                            plain_priv_keys.push(SecretKey::from_str(&input.private_key).unwrap())
+                        }
+                    },
                     Ok(None) => (),
                     Err(e) => panic!("Problem parsing the input: {:?}", e),
                 }
             }
-            if input_priv_keys.len() == 0 {
+
+            if taproot_priv_keys.is_empty() && plain_priv_keys.is_empty() {
                 continue;
             }
 
             // we drop the amounts from the test here, since we don't work with amounts
             // the wallet should make sure the amount sent are correct
-            let silent_addresses = decode_recipients(&given.recipients);
 
-            // as an alternative, we could first multiply each input priv key with the input hash
-            // that way, we never expose the sk to our library
-            let partial_secret = calculate_partial_secret(&input_priv_keys, &outpoints).unwrap();
-            let outputs = generate_recipient_pubkeys(silent_addresses, partial_secret).unwrap();
+            // We look up the number of expected outputs for each address
+            let recipients: Vec<SilentpaymentsRecipient> = decode_recipients(given.recipients);
 
-            for output_pubkeys in &outputs {
-                for pubkey in output_pubkeys.1 {
-                    sending_outputs.insert(hex::encode(pubkey.serialize()));
-                }
+            let smallest_outpoint = get_smallest_outpoint(outpoints).unwrap();
+
+            let taproot_seckeys = taproot_priv_keys.iter().collect::<Vec<&Keypair>>();
+            let plain_seckeys = plain_priv_keys.iter().collect::<Vec<&SecretKey>>();
+
+            println!("recipients: {:?}", recipients);
+
+            let outputs = silentpayments_sender_create_outputs(
+                &secp, 
+                recipients.iter().collect::<Vec<&SilentpaymentsRecipient>>().as_mut_slice(), 
+                &smallest_outpoint, 
+                Some(taproot_seckeys.as_slice()),
+                Some(plain_seckeys.as_slice())
+            ).unwrap();
+
+            for output_pubkey in &outputs {
+                sending_outputs.insert(hex::encode(output_pubkey.serialize()));
             }
+
+            println!("sending_outputs: {:?}", sending_outputs);
+
+            // if test_case.comment == "Multiple outputs: multiple outputs, multiple recipients" {
+            //     assert!(false);
+            // }
+            
             assert!(expected.outputs.iter().any(|candidate_set| {
+                println!("candidate_set: {:?}", candidate_set);
                 sending_outputs
                     .iter()
                     .all(|output| candidate_set.contains(output))
@@ -103,17 +129,16 @@ mod tests {
             let B_spend = b_spend.public_key(&secp);
             let B_scan = b_scan.public_key(&secp);
 
-            let change_label = Label::new(b_scan, 0);
-            let mut sp_receiver = Receiver::new(0, B_scan, B_spend, change_label, NETWORK).unwrap();
+            let change_label = silentpayments_recipient_create_label_tweak(&secp, &b_scan, 0).unwrap();
+            let change_B_spend = silentpayments_recipient_create_labelled_spend_pubkey(&secp, &B_spend, &change_label.pubkey).unwrap();
+            let change_address = SilentPaymentAddress::new(B_scan, change_B_spend, NETWORK, VERSION).unwrap();
 
             let outputs_to_check = decode_outputs_to_check(&given.outputs);
 
-            let outpoints: Vec<(String, u32)> = given
-                .vin
-                .iter()
-                .map(|vin| (vin.txid.clone(), vin.vout))
-                .collect();
-            let mut input_pub_keys = Vec::new();
+            let smallest_outpoint = get_smallest_outpoint(given.vin.iter().map(|vin| (vin.txid.clone(), vin.vout)).collect()).unwrap();
+
+            let mut taproot_pubkeys: Vec<XOnlyPublicKey> = vec![];
+            let mut legacy_pubkeys: Vec<PublicKey> = vec![];
             for input in given.vin {
                 let script_sig = hex::decode(&input.scriptSig).unwrap();
                 let txinwitness_bytes = hex::decode(&input.txinwitness).unwrap();
@@ -122,35 +147,46 @@ mod tests {
                 let script_pub_key = hex::decode(&input.prevout.scriptPubKey.hex).unwrap();
 
                 match get_pubkey_from_input(&script_sig, &txinwitness, &script_pub_key) {
-                    Ok(Some(pubkey)) => input_pub_keys.push(pubkey),
+                    Ok(Some((pubkey, is_p2tr))) => {
+                        if is_p2tr {
+                            taproot_pubkeys.push(pubkey.x_only_public_key().0);
+                        } else {
+                            legacy_pubkeys.push(pubkey);
+                        }
+                    }
                     Ok(None) => (),
                     Err(e) => panic!("Problem parsing the input: {:?}", e),
                 }
             }
-            if input_pub_keys.len() == 0 {
-                continue;
-            };
 
-            let input_pub_keys: Vec<&PublicKey> = input_pub_keys.iter().collect();
+            let xonly_pubkeys = taproot_pubkeys.iter().collect::<Vec<&XOnlyPublicKey>>();
+            let plain_pubkeys = legacy_pubkeys.iter().collect::<Vec<&PublicKey>>();
+
+            if xonly_pubkeys.is_empty() && plain_pubkeys.is_empty() {
+                continue;
+            }
+
+            let mut labels: BiMap<PublicKey, [u8; 32]> = BiMap::new();
 
             for label_int in &given.labels {
-                let label = Label::new(b_scan, *label_int);
-                sp_receiver.add_label(label).unwrap();
+                let label = silentpayments_recipient_create_label_tweak(&secp, &b_scan, *label_int).unwrap();
+                labels.insert(label.pubkey, label.label_tweak);
             }
 
             let mut receiving_addresses: HashSet<String> = HashSet::new();
             // get receiving address for no label
-            receiving_addresses.insert(sp_receiver.get_receiving_address());
+            receiving_addresses.insert(SilentPaymentAddress::new(B_scan, B_spend, NETWORK, VERSION).unwrap().to_string());
 
             // get receiving addresses for every label
-            let labels = sp_receiver.list_labels();
-            for label in &labels {
+            for (label, tweak) in &labels {
+                let labelled_B_spend = silentpayments_recipient_create_labelled_spend_pubkey(&secp, &B_spend, label).unwrap();
+                let labelled_address = SilentPaymentAddress::new(B_scan, labelled_B_spend, NETWORK, VERSION).unwrap();
                 receiving_addresses
-                    .insert(sp_receiver.get_receiving_address_for_label(label).unwrap());
+                    .insert(labelled_address.to_string());
             }
 
             if !&given.labels.iter().any(|l| *l == 0) {
-                receiving_addresses.remove(&sp_receiver.get_change_address());
+                receiving_addresses.remove(&change_address.to_string());
             }
 
             let set1: HashSet<_> = receiving_addresses.iter().collect();
@@ -160,23 +196,35 @@ mod tests {
             // to the expected addresses
             assert_eq!(set1, set2);
 
-            let tweak_data = calculate_tweak_data(&input_pub_keys, &outpoints).unwrap();
-            let ecdh_shared_secret = calculate_ecdh_shared_secret(&tweak_data, &b_scan);
+            // let tweak_data = calculate_tweak_data(&input_pub_keys, &outpoints).unwrap();
+            // let ecdh_shared_secret = calculate_ecdh_shared_secret(&tweak_data, &b_scan);
+            let public_data = SilentpaymentsPublicData::create(
+                &secp, 
+                &smallest_outpoint, 
+                Some(xonly_pubkeys.as_slice()), 
+                Some(plain_pubkeys.as_slice())
+            ).unwrap();
 
-            let scanned_outputs_received = sp_receiver
-                .scan_transaction(&ecdh_shared_secret, outputs_to_check)
-                .unwrap();
+            let labels_store = LabelsStore::new(labels);
+            let scanned_outputs_received = silentpayments_recipient_scan_outputs(
+                &secp, 
+                outputs_to_check.iter().collect::<Vec<&XOnlyPublicKey>>().as_slice(), 
+                &b_scan, 
+                &public_data, 
+                &B_spend, 
+                label_lookup_callback, 
+                labels_store
+            ).unwrap();
 
-            let key_tweaks: Vec<Scalar> = scanned_outputs_received
+            let key_tweaks: Vec<[u8; 32]> = scanned_outputs_received
                 .into_iter()
-                .flat_map(|(_, map)| {
-                    let mut ret: Vec<Scalar> = vec![];
-                    for l in map.into_values() {
-                        ret.push(l);
-                    }
-                    ret
+                .map(|output| {
+                    output.get_tweak()
                 })
                 .collect();
+
+
+            println!("key_tweaks: {:?}", key_tweaks);
 
             let res = verify_and_calculate_signatures(key_tweaks, b_spend).unwrap();
             assert!(expected.outputs.len() == res.len());
